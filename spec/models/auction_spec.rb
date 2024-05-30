71aa3b23654da8847a7b25716c5f4675b4acf773
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-05-30 17:06:45 +0100

    Add subject object and write remaining tests
    Subject object is there to have all of our tests run under
    The other tests for each example is written
    Notice that each test manipulates the subject object to create the state
    that will result in a validation error

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,16 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
-  it "is valid with valid attributes" do
-    expect(Auction.new).to be_valid
+
+  subject {
+    described_class.new(title: 'Anything',
+                        description: 'Lorem Ipsum',
+                        start_date: DateTime.now,
+                        end_date: DateTime.now + 1.week,
+                       ) #user_id: 1)
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
   end
 
-  it "is not valid without a title" do
-    auction = Auction.new(title: nil)
-    expect(auction).to_not be_valid
+  it 'is not valid without a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
   end
-  
-  it "is not valid without a description"
-  it "is not valid without a start date"
-  it "is not valid without a end_date"
+
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a start date' do
+    subject.start_date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a end_date' do
+    subject.end_date = nil
+    expect(subject).to_not be_valid
+  end
 end
