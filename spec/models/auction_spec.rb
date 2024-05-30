ea9bcf9850b54b495b02f4c2a5bacc48d424e95f
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-05-30 14:58:26 +0100

    Validations for auctions and 2 tests

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(Auction.new).to be_valid
+  end
+
+  it "is not valid without a title" do
+    auction = Auction.new(title: nil)
+    expect(auction).to_not be_valid
+  end
+  
+  it "is not valid without a description"
+  it "is not valid without a start date"
+  it "is not valid without a end_date"
 end
